1aaa641e8fe17e09948a5f06cfe6259f5a013845
Никита <siriusnick@gmail.com>
2013-02-04 10:28:14 +0400

    Update include/types.h
    Fix types for gcc 4.4.6 compatible

--- include/types.h
+++ include/types.h
@@ -23,35 +23,58 @@
 
 #ifdef __INT8_TYPE__
 typedef __INT8_TYPE__ int8_t;
+#else
+typedef char int8_t;
 #endif
+
 #ifdef __INT16_TYPE__
 typedef __INT16_TYPE__ int16_t;
+#else
+typedef short int int16_t;
 #endif
+
 #ifdef __INT32_TYPE__
 typedef __INT32_TYPE__ int32_t;
+#else
+typedef int int32_t;
 #endif
+
 #ifdef __INT64_TYPE__
 typedef __INT64_TYPE__ int64_t;
+#else
+typedef long long int64_t;
 #endif
+
 #ifdef __UINT8_TYPE__
 typedef __UINT8_TYPE__ uint8_t;
+#else
+typedef unsigned char uint8_t;
 #endif
+
 #ifdef __UINT16_TYPE__
 typedef __UINT16_TYPE__ uint16_t;
+#else
+typedef unsigned short int uint16_t;
 #endif
+
 #ifdef __UINT32_TYPE__
 typedef __UINT32_TYPE__ uint32_t;
+#else
+typedef unsigned int uint32_t;
 #endif
+
 #ifdef __UINT64_TYPE__
 typedef __UINT64_TYPE__ uint64_t;
+#else
+typedef unsigned long long uint64_t;
 #endif
 
 #ifdef __SIZE_TYPE__
-typedef __SIZE_TYPE__ 	size_t;
+typedef unsigned long int 	size_t;
 #endif
 
+#ifdef __INT_LEAST8_TYPE__
 /* 7.8.1.2 Minimum-width integer types */
-
 typedef __INT_LEAST8_TYPE__ int_least8_t;
 typedef __INT_LEAST16_TYPE__ int_least16_t;
 typedef __INT_LEAST32_TYPE__ int_least32_t;
@@ -62,7 +85,6 @@ typedef __UINT_LEAST32_TYPE__ uint_least32_t;
 typedef __UINT_LEAST64_TYPE__ uint_least64_t;
 
 /* 7.8.1.3 Fastest minimum-width integer types */
-
 typedef __INT_FAST8_TYPE__ int_fast8_t;
 typedef __INT_FAST16_TYPE__ int_fast16_t;
 typedef __INT_FAST32_TYPE__ int_fast32_t;
@@ -71,14 +93,20 @@ typedef __UINT_FAST8_TYPE__ uint_fast8_t;
 typedef __UINT_FAST16_TYPE__ uint_fast16_t;
 typedef __UINT_FAST32_TYPE__ uint_fast32_t;
 typedef __UINT_FAST64_TYPE__ uint_fast64_t;
+#endif
 
 /* 7.8.1.4 Integer types capable of holding object pointers */
 
 #ifdef __INTPTR_TYPE__
 typedef __INTPTR_TYPE__ intptr_t;
+#else
+typedef int             *intptr_t;
 #endif
+
 #ifdef __UINTPTR_TYPE__
 typedef __UINTPTR_TYPE__ uintptr_t;
+#else
+typedef unsigned int     *uintptr_t;
 #endif
 
 typedef void	 		*ptr_t;
